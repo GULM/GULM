ec4877d021810ad6c687dd654b00c130e0755bda
jefferson oliveira <jeffersonmourak@jeffersons-MacBook.local>
2016-04-10 17:04:57 -0300

    static files and corrections

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,12 @@ var uglify = require('gulp-uglifyjs');
 var copy = require('gulp-copy');
 var dependencies = require("./dependencies.json");
 
-gulp.task("copy", function () {
+gulp.task("copyS", function () {
 	return gulp.src(dependencies.scripts)
 				.pipe(gulp.dest("static/lib/scripts"));
+});
+
+gulp.task("copyC", function () {
+	return gulp.src(dependencies.styles)
+				.pipe(gulp.dest("static/lib/styles"));
 });
